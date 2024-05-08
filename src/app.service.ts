e75c0d29c545709021a4d047c32b362062bc8be8
Matheus Monteiro <msmonteir@gmail.com>
2024-05-08 07:22:35 -0300

    FEAT - changed promise to receive JSON for expect for a CPF and a CNPJ

--- src/app.service.ts
+++ src/app.service.ts
@@ -25,6 +25,6 @@ export class AppService {
     return {
       "data": data,
       "associated": isAssociate
-    };
+    }
   }
-} 
+}
--- src/register-payload.dto.ts
+++ src/register-payload.dto.ts
@@ -1,10 +1,4 @@
 export class RegisterDTO {
-  name: string
-  nickname: string
-  corporate: string
-  fantasyName: string
   cnpj: string
-  email: string
-  phone: string
   cpf: string
 }
