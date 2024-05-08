b44937d417883c3ff5c260bba3d22bdc5ba41df7
deviego <deviego4@gmail.com>
2024-05-08 10:38:47 -0300

    remove app.enablecors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true });
-  app.enableCors()
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
