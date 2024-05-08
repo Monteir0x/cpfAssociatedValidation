deb73dfa24d5e38c1917e41b4978c8ee709cdaac
Matheus Monteiro <msmonteir@gmail.com>
2024-05-08 10:13:51 -0300

    FIX - enabled CORS with options

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    methods: ['GET', 'POST'],
+    origin: true
+  })
   await app.listen(3000);
 }
 bootstrap();
