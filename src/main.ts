3250ad415b643448098db13b8ba92774cb778fd9
deviego <deviego4@gmail.com>
2024-05-08 10:35:54 -0300

    without objet in enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true });
-  app.enableCors({
-    credentials: true,
-    methods: ['GET', 'POST'],
-    origin: true
-  })
+  app.enableCors()
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
