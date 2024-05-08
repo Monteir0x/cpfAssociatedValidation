8c712ee3731ff648a81d3e8aabd2c2df250c8d4a
root <root@DESKTOP-VUNKL1M>
2024-05-08 10:25:57 -0300

    app.use to cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ async function bootstrap() {
     methods: ['GET', 'POST'],
     origin: true
   })
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
